6765b26314e154d409e01783201aa3e3891d80f4
Kiptoo Rugut <mccrug97@gmail.com>
2022-04-21 16:06:41 +0300

    it will add a class that represents table or collection in our DB, and where every attribute of the class is a field of the table or collection.

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,145 @@
+from operator import imod
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50, default='name')
+    image = CloudinaryField('image',null=True)
+    description= models.TextField(max_length=300,null=True)
+    occupants = models.IntegerField(default=0)
+    location = models.CharField(max_length=60,null=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    health_tell = models.IntegerField(null=True, blank=True)
+    police_tell = models.IntegerField(null=True, blank=True)
+    area_administrator = models.CharField(max_length=100,null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def create_neigborhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighborhood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_neighborhood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    def __str__(self):
+        return self.name
+    
+class Profile(models.Model):
+    name = models.CharField(max_length=50,default='name')
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField(max_length=300,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE,related_name='neighbors',null=True)
+
+    def str(self):
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+
+    def update_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+    
+
+class Business(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    image=CloudinaryField('image',null=True)
+    email = models.EmailField(max_length=50, null= True)
+    description = models.TextField(blank=True, null=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(default='2018-11-20T15:58:44-06:00',null=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # create business
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    def update_business(self):
+        self.update()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        business = cls.objects.filter(name__icontains=search_term)
+        return business
+
+    def __str__(self):
+        return self.name
+
+#Post model
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, related_name='hood_post',null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def create_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    def update_post(self):
+        self.update()
+
+    def __str__(self):
+        return self.title
